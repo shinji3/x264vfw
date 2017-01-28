83f2cf671114c08181ff3e2867096f9285b383df
shinji3 <renrakusaki2003@yahoo.co.jp>
2017-01-28 16:23:41 +0900

    ビルドスクリプトのiconvとnvencを無効にした

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -1,4 +1,4 @@
 cd ffmpeg 
-./configure --enable-runtime-cpudetect --disable-programs --disable-doc --disable-avdevice --disable-avformat --disable-swresample --disable-avfilter --enable-dxva2 --disable-everything --enable-decoder=h264 --disable-debug
+./configure --enable-runtime-cpudetect --disable-programs --disable-doc --disable-avdevice --disable-avformat --disable-swresample --disable-avfilter --disable-iconv --disable-nvenc --enable-dxva2 --disable-everything --enable-decoder=h264 --disable-debug
 make
 cd ..
